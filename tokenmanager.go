ef779d7899b01ff98a1737a69c2cd3074c986157
Janilton Maciel <janilton@corp.globo.com>
2015-08-23 16:22:48 -0300

    Added retry

--- tokenmanager.go
+++ tokenmanager.go
@@ -4,13 +4,13 @@ import (
 	"encoding/base64"
 	"time"
 
+	"gitlab.globoi.com/bastian/falkor/errors"
+
 	"github.com/facebookgo/stackerr"
 	"github.com/franela/goreq"
 )
 
-const (
-	GrantType = "grant_type=client_credentials"
-)
+const GrantType = "grant_type=client_credentials"
 
 type TokenManager interface {
 	GetToken() (*Token, error)
@@ -43,32 +43,54 @@ func NewTokenManager(tokenEndPoint, clientId, clientSecret string, debug bool, t
 	return tm
 }
 
+func SetDefaultTokenManager(t TokenManager) {
+	defaultTokenManager = t
+}
+
 func (tm *OAuthTokenManager) GetToken() (*Token, error) {
 
 	if tm.token == nil || !tm.token.isValid() {
-		resp, err := goreq.Request{
-			Method:      "POST",
-			ContentType: "application/x-www-form-urlencoded",
-			Uri:         tm.TokenEndPoint,
-			Body:        GrantType,
-			ShowDebug:   tm.Debug,
-			Timeout:     tm.Timeout,
-		}.WithHeader("Authorization", tm.Authorization).Do()
+		var err error
+		var resp *goreq.Response
 
-		if err != nil {
-			return nil, stackerr.Wrap(err)
-		}
+		maxRetries := 2
+		for i := 0; i < maxRetries; i++ {
+			if resp, err = requestToken(tm); err != nil {
+				return nil, stackerr.Wrap(err)
+			}
 
-		defer resp.Body.Close()
-		tm.token, err = newToken(resp.Body)
-		if err != nil {
-			return nil, err
+			// 200 and 300 level errors are considered success and we are done
+			if resp.StatusCode < 400 {
+				defer resp.Body.Close()
+				tm.token, err = newToken(resp.Body)
+				if err != nil {
+					return nil, err
+				}
+				return tm.token, nil
+			}
+
+			// wait 50ms
+			time.Sleep(50 * time.Millisecond)
 		}
+		return nil, errors.NewHttpError(resp.StatusCode, "Não foi possível recupera o token")
 	}
 
 	return tm.token, nil
 }
 
-func SetDefaultTokenManager(t TokenManager) {
-	defaultTokenManager = t
+func requestToken(tm *OAuthTokenManager) (*goreq.Response, error) {
+	resp, err := goreq.Request{
+		Method:      "POST",
+		ContentType: "application/x-www-form-urlencoded",
+		Uri:         tm.TokenEndPoint,
+		Body:        GrantType,
+		ShowDebug:   tm.Debug,
+		Timeout:     tm.Timeout,
+	}.WithHeader("Authorization", tm.Authorization).Do()
+
+	if err != nil {
+		return nil, stackerr.Wrap(err)
+	}
+
+	return resp, nil
 }
